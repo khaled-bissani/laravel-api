80daec73e749b6f76f46b2b3aa5966eba236481c
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-30 22:46:06 +0300

    Backend <> Route: Adding all the routes for all the api required

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http \Controllers\MyController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['prefix' => 'test'], function(){
+    Route::get("/api1", [MyController::class, 'stringSorting']);
+    Route::get("/api2", [MyController::class, 'numberPlace']);
+    Route::get("/api3", [MyController::class, 'decimalToBinary']);
+    Route::get("/api4", [MyController::class, 'prefixNotation']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
