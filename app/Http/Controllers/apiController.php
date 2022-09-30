3a027e1f55d66825d5d17804aee7096db8a59a0b
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-30 22:48:42 +0300

    Backend <> Api: Adding all the api function in the apiController

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class apiController extends Controller
 {
-    //
+    // First Api
+    function stringSorting(){
+    }
+    
+    // Second Api
+    function numberPlace(){   
+    }
+
+    // Third Api
+    function decimalToBinary(){
+    }
+    
+    // Fourth Api
+    function prefixNotation(){
+    }
 }
