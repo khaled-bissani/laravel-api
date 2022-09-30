0354262010d736859bdfdb9a35c418cc9068f306
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-30 22:54:26 +0300

    Backend <> Third api: Creating an Api that translates from Human to Programer number

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -25,6 +25,23 @@ class apiController extends Controller
 
     // Third Api
     function decimalToBinary(){
+        $string = $request->string;
+        $binary_array=array();
+        for($i=0; $i<strlen($string);$i++){
+            if(is_numeric($string[$i])){
+                $number=$string[$i];
+                $binary=0;
+                $remaning;
+                $order=1;
+                while($number!=0){
+                    $remaning= $number%2;
+                    $number=intdiv($number,2);
+                    $binary=$binary+ $remaning*$order;
+                    $order=$order*10;
+                }
+                return substr($string,0,$i).$binary.substr($string,$i+1,-1);
+            }
+        }
     }
     
     // Fourth Api
