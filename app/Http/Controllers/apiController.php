7b31ea7a6f76a1c286b12640f0a7764c6a013f91
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-30 22:52:18 +0300

    Backend <> Second api: Creating an Api that returns each place value in the number

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -9,9 +9,18 @@ class apiController extends Controller
     // First Api
     function stringSorting(){
     }
-    
+
     // Second Api
-    function numberPlace(){   
+    function numberPlace(Request $request){   
+        $num = $request->num;
+        $number_place=array();
+        $place=1;
+        while($num!=0){
+            $result = array_merge([$place*($num%10)], $number_place);
+            $place=$place*10;
+            $num=floor($num/ 10);
+            print_r ($result);
+        }   
     }
 
     // Third Api
